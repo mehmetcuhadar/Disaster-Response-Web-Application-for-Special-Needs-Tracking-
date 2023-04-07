2ce7971e99428ac12f84fc813d3c89bbb2c51121
mehmetcuhadar <cuhadarrmehmet@gmail.com>
2023-04-07 13:21:33 +0300

    User Authentication Added

--- Authentication-Service/auth.js
+++ Authentication-Service/auth.js
@@ -1,93 +1,83 @@
 const mongoose = require('mongoose')
 const express = require('express')
+const fs = require('fs');
+const https = require('https');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
 const app = express()
 const User = require("./user.js")
 const dbURL = 'mongodb+srv://mcuhadar18:ee8iLI9KF5HpYpoM@adress.qai6yhk.mongodb.net/auth-log?retryWrites=true&w=majority'
+const config = require("./config.js");
+const pepper = config.pepper;
 
 mongoose.connect(dbURL, {useNewUrlParser : true, useUnifiedTopology: true})
-.then((result) => app.listen(3002))
+.then((result) => {
+  https.createServer({
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./cert.pem')
+  }, app).listen(3002, function () {
+    console.log('Server listening on https://localhost:3002');
+  });
+})
 .catch((err) => console.log(err))
 
+app.use(express.json());
 app.use(cors());
 
-app.get('/addUser', (req, res) => {
-  const input = new User({
-    name: req.query.name || "", // Use the value of il_title query parameter 
-    surname: req.query.surname || "", // Use the value of ilce_title query parameter 
-    phone: req.query.phone || "", // Use the value of mahalle_title query parameter 
-    username: req.query.username || "",  // Use the value of sokak_cadde_title query parameter 
-    password: req.query.password || "",
-  });
-  
-  input.save()
-    .then((result) => { 
-      res.send(true + result);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send('An error occurred while saving the input.');
-    });
-});
-
-
-
-app.get('/getPassword', (req, res) => {
-  const username = req.query.username;
-
-  User.findOne({ username: username })
-    .then((user) => {
-      if (!user) {
-        res.status(404).send('User not found');
-        return;
-      }
-      // Encrypt the password using bcryptjs library
-      bcrypt.genSalt(10, function(err, salt) {
-        bcrypt.hash(user.password, salt, function(err, hash) {
-          if (err) {
-            console.log(err);
-            res.status(500).send('An error occurred while encrypting the password.');
-          } else {
-            res.send(hash);
-          }
+app.post('/addUser', async (req, res) => {
+  //const { name, surname, phone, username, password } = req.body;
+  const [name, surname, phone, username, password] = ["Doğukan", "Soyuyüce", "123132", "dsoyuyuce", "111111"];
+  const user = await User.findOne({ username });
+  if (!user) {
+    // Handle user not found error
+    bcrypt.hash(password + pepper, 10, (err, hash) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send('An error occurred while hashing the password.');
+      } else {
+        const input = new User({
+          name,
+          surname,
+          phone,
+          username,
+          password: hash,
         });
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send('An error occurred while retrieving the password.');
+        input.save()
+          .then((result) => {
+            res.send(result);
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(500).send('An error occurred while saving the input.');
+          });
+      }
     });
+    
+  }else{
+    console.log('User is defined');
+  }
+  
 });
 
 
-
-function login() {
-    var username = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
+app.post('/login', async (req, res) => {
   
-    // check if username and password match admin credentials
-    if (username === "" || password === "") {
-        var warningMessage = document.getElementById("warning-message");
-        warningMessage.style.display = "block";
-        warningMessage.innerHTML = "Please enter your username and password.";
-        return; 
-    }
+  const { username, password } = req.body;
 
-    if (username === "a" && password === "a") {
-        console.log("Redirecting to dashboard...");
-        window.location.href = "loading.html";
 
-        setTimeout(function() {
-			console.log("Redirecting to admin dashboard...");
-			// Redirect to the admin dashboard after the delay
-			window.location.href = "index.html";
-		}, 5); 
-
-    } else {
-      console.log("Redirecting to dashboard...");
-      var warningMessage = document.getElementById("warning-message");
-      warningMessage.style.display = "block";
-      warningMessage.innerHTML = "Invalid username or password";
-    }
+  // Find the user in MongoDB by username
+  const user = await User.findOne({ username });
+  if (!user) {
+    // Handle user not found error
+    console.log("User not found")
+    return res.send(false);
   }
+  // Compare the user's input password to the hashed password stored in MongoDB
+  const isPasswordValid = await bcrypt.compare(password + pepper, user.password);
+  if (isPasswordValid) {
+    // Handle successful login
+    res.send(true);
+  } else {
+    res.send(false);
+  }
+});
---
+++ Authentication-Service/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    pepper: "2912djasjc802u1kjdlasjas9d0as9jc12n9wc1l2j12",
+    // Other configuration options...
+  };
+  
