b2eecc931be694567b58a392f84a1c3ac2f955bd
HaticeNurYildiz <72516463+HaticeNurYildiz@users.noreply.github.com>
2023-05-21 14:44:07 +0300

    Update main.py

--- Twitter-Service/main.py
+++ Twitter-Service/main.py
@@ -3,12 +3,13 @@ import tweepy
 
 # Model
 ner_pipe = pipeline("token-classification", "hny17/finetune_ner")
+intent_pipe = pipeline("text-classification", "hny17/finetune_req")
 
 # Twitter
-api_key = "A6lWBmbap4Jzwy0oVqhoCoTs8"
-api_secret = "PBJ6GWMtVUd6SQsYZgtZ4Mifiyzvvm5JOmNkbRG3cuzz9BEiNq"
-access_token = "1649053227793645582-ocEQ5aC3ugUeIDaC3MOIOAjQlkiV0H"
-access_token_secret = "dKh01sWvEHguW9LAUg1yoo9kT7MieAw2wZcjTGculutub"
+api_key = "yx52sCQywkuXwLWXOwBBHKku8"
+api_secret = "2eoL6VqIPUvarBGkHPfqdtCnObGhasVOC1MGEOF0wBlmOjMdEl"
+access_token = "1649053227793645582-vwuu3NFaAWsNogxdZXTmf8ptIcEoEl"
+access_token_secret = "gg8m1JxmTKdG6plyPyDmOXv0wKqJph91ZER1pgy1Zppga"
 
 auth = tweepy.OAuthHandler(api_key, api_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -16,10 +17,13 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 keyword = "#acilafadyardım"
-num_tweets = 3
+num_tweets = 5
+
 
 for tweet in tweepy.Cursor(api.search_tweets, q=keyword).items(num_tweets):
     text_without_hashtags = ''.join(word for word in tweet.text if not word.startswith('#'))
     print(text_without_hashtags)
-    prediction = ner_pipe(text_without_hashtags)
-    print(prediction)
+    location = ner_pipe(text_without_hashtags)
+    print(location)
+    request = intent_pipe(text_without_hashtags)
+    print(request)
