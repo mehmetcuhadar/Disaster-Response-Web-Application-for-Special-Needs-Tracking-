f385950c9915c311ec1d183e6ddbdd7d8892f178
HaticeNurYildiz <72516463+HaticeNurYildiz@users.noreply.github.com>
2023-05-24 22:53:00 +0300

    Update main.py

--- Twitter-Service/main.py
+++ Twitter-Service/main.py
@@ -1,29 +1,46 @@
 from transformers import pipeline
 import tweepy
 
-# Model
+# Models
 ner_pipe = pipeline("token-classification", "hny17/finetune_ner")
 intent_pipe = pipeline("text-classification", "hny17/finetune_req")
 
-# Twitter
-api_key = "yx52sCQywkuXwLWXOwBBHKku8"
-api_secret = "2eoL6VqIPUvarBGkHPfqdtCnObGhasVOC1MGEOF0wBlmOjMdEl"
-access_token = "1649053227793645582-vwuu3NFaAWsNogxdZXTmf8ptIcEoEl"
-access_token_secret = "gg8m1JxmTKdG6plyPyDmOXv0wKqJph91ZER1pgy1Zppga"
+# Twitter tokens
+bearer_token = "AAAAAAAAAAAAAAAAAAAAAFGWmwEAAAAAWyB6K98ZlDcPeoGipF1boVb0lcU%3DYS7g9Z2Ny8M1cxaH4vI7s8OyLqBanO2g57pIgswPaeYhsrT44T"
+api_key = "tPvhOVJ9cGgHR4yGERZyx1iya"
+api_secret = "HP9V9JZttSkns1IeMa844glzVODofe7k8olMRrJdrYPZ5vJKMv"
+access_token = "1649053227793645582-9a0B2C0s3ReYlCzy9J2KvmRzSuRP6K"
+access_token_secret = "QmIs74ulBcs4gbxim2662rs7TBJwZN7ADVtwuP7sF0mYC"
 
 auth = tweepy.OAuthHandler(api_key, api_secret)
 auth.set_access_token(access_token, access_token_secret)
-
-api = tweepy.API(auth)
+# api = tweepy.API(auth)
+api = tweepy.API(auth, wait_on_rate_limit=True)
 
 keyword = "#acilafadyardım"
 num_tweets = 5
+time_window = datetime.timedelta(minutes=1)
+timezone = pytz.timezone('Europe/Istanbul')
+processed_tweet_ids = set()
 
 
-for tweet in tweepy.Cursor(api.search_tweets, q=keyword).items(num_tweets):
-    text_without_hashtags = ''.join(word for word in tweet.text if not word.startswith('#'))
-    print(text_without_hashtags)
-    location = ner_pipe(text_without_hashtags)
-    print(location)
-    request = intent_pipe(text_without_hashtags)
-    print(request)
+while True:
+    current_time = datetime.datetime.now(timezone)
+
+    for tweet in tweepy.Cursor(api.search_tweets, q=keyword).items():
+        tweet_time = tweet.created_at.astimezone(timezone)
+        time_difference = current_time - tweet_time
+
+        if tweet.id in processed_tweet_ids:
+            continue
+
+        if time_difference <= time_window:
+            text_without_hashtags = ''.join(word for word in tweet.text if not word.startswith('#'))
+            print(text_without_hashtags)
+            location = ner_pipe(text_without_hashtags)
+            print(location)
+            request = intent_pipe(text_without_hashtags)
+            print(request)
+            processed_tweet_ids.add(tweet.id)
+
+    time.sleep(2)
