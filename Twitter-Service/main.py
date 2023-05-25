ecbe8d90f5d4ed143451b294367f2eec17a3c674
HaticeNurYildiz <72516463+HaticeNurYildiz@users.noreply.github.com>
2023-05-25 21:36:37 +0300

    Update main.py

--- Twitter-Service/main.py
+++ Twitter-Service/main.py
@@ -1,7 +1,12 @@
+import time
+import json
 from transformers import pipeline
 import tweepy
+import datetime
+import pytz
+from pymongo import MongoClient
 
-# Models
+# Model
 ner_pipe = pipeline("token-classification", "hny17/finetune_ner")
 intent_pipe = pipeline("text-classification", "hny17/finetune_req")
 
@@ -18,11 +23,13 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
 keyword = "#acilafadyardım"
-num_tweets = 5
 time_window = datetime.timedelta(minutes=1)
 timezone = pytz.timezone('Europe/Istanbul')
 processed_tweet_ids = set()
 
+client = MongoClient('mongodb+srv://mcuhadar18:ee8iLI9KF5HpYpoM@adress.qai6yhk.mongodb.net/input-log?retryWrites=true&w=majority')
+db = client['input_log']
+collection = db['twitter_data']
 
 while True:
     current_time = datetime.datetime.now(timezone)
@@ -36,11 +43,71 @@ while True:
 
         if time_difference <= time_window:
             text_without_hashtags = ''.join(word for word in tweet.text if not word.startswith('#'))
-            print(text_without_hashtags)
+            # print(text_without_hashtags)
             location = ner_pipe(text_without_hashtags)
-            print(location)
+            # print(location)
             request = intent_pipe(text_without_hashtags)
-            print(request)
+            # print(request)
             processed_tweet_ids.add(tweet.id)
 
-    time.sleep(2)
+            location_data = {
+                'il_title': '',
+                'ilce_title': '',
+                'mahalle_title': '',
+                'sokak_cadde_title': '',
+                'site_title': '',
+                'apartman_title': '',
+                'tel_number': ''
+            }
+
+            for entity in location:
+                if entity['entity'] == 'B-il':
+                    location_data['il_title'] = entity['word']
+                elif entity['entity'] == 'I-il':
+                    location_data['il_title'] = ' ' + entity['word']
+                elif entity['entity'] == 'B-ilce':
+                    location_data['ilce_title'] = entity['word']
+                elif entity['entity'] == 'I-ilce':
+                    location_data['ilce_title'] = ' ' + entity['word']
+                elif entity['entity'] == 'B-mahalle':
+                    location_data['mahalle_title'] = entity['word']
+                elif entity['entity'] == 'I-mahalle':
+                    location_data['mahalle_title'] = ' ' + entity['word']
+                elif entity['entity'] == 'B-sokak':
+                    location_data['sokak_cadde_title'] = entity['word']
+                elif entity['entity'] == 'I-sokak':
+                    location_data['sokak_cadde_title'] = ' ' + entity['word']
+                elif entity['entity'] == 'B-site':
+                    location_data['site_title'] = entity['word']
+                elif entity['entity'] == 'I-site':
+                    location_data['site_title'] = ' ' + entity['word']
+                elif entity['entity'] == 'B-apartman':
+                    location_data['apartman_title'] = entity['word']
+                elif entity['entity'] == 'I-apartman':
+                    location_data['apartman_title'] = ' ' + entity['word']
+                elif entity['entity'] == 'B-tel':
+                    location_data['tel_number'] = entity['word']
+                elif entity['entity'] == 'I-tel':
+                    location_data['tel_number'] = ' ' + entity['word']
+
+            input_data = {
+                'id': str(tweet.id),
+                'il_title': location_data['il_title'],
+                'ilce_title': location_data['ilce_title'],
+                'mahalle_title': location_data['mahalle_title'],
+                'sokak_cadde_title': location_data['sokak_cadde_title'],
+                'site_title': location_data['site_title'],
+                'apartman_title': location_data['apartman_title'],
+                'tel_number': location_data['tel_number'],
+                'ihtiyac_title': request['label'],
+                'add_info': request['add_info'],
+                'created_at': tweet_time,
+                'status': 'active'
+            }
+
+            collection.insert_one(input_data)
+            print("Data inserted into MongoDB")
+
+            processed_tweet_ids.add(tweet.id)
+
+    time.sleep(3)
